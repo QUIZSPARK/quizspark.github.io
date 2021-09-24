497f93212105ca468b9e2d1421f7f442cd661822
QUIZSPARK <91334387+QUIZSPARK@users.noreply.github.com>
2021-09-24 20:48:16 +0530

    quizspark
    game

---
+++ js/questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Planet which is small in our Solar System?",
+    answer: "Mercury",
+    options: [
+      "Uranus",
+      "Jupiter",
+      "Earth",
+      "Mercury"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Planet which has only one Moon in its Orbit?",
+    answer: "Earth",
+    options: [
+      "Venus",
+      "Earth",
+      "Jupiter",
+      "Mars"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Planet which is Nearest to the Sun?",
+    answer: "Mercury",
+    options: [
+      "Saturn",
+      "Mars",
+      "Mercury",
+      "Uranus"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many Planets are there in our Solar System?",
+    answer: "8",
+    options: [
+      "8",
+      "6",
+      "9",
+      "4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is the First Female Astronaut from India?",
+    answer: "Kalpana chawla",
+    options: [
+      "Sunita williams",
+      "Kalpana chawla",
+      "Lisa norwak",
+      "None of these"
+    ]
+  },
+  {
+    numb: 6,
+    question: "If an Astronaut goes in Space he/she will Observe the Sky as?",
+    answer: "Black",
+    options: [
+      "White",
+      "Red",
+      "Blue",
+      "Black"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Planets which have no Moon?",
+    answer: "Mercury And Venus",
+    options: [
+      "Pluto And Mercury",
+      "Mars And Venus",
+      "Mercury And Venus",
+      "Pluto And Mars"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The Coldest Planet in our Solar System?",
+    answer: "Uranus",
+    options: [
+      "Pluto",
+      "Uranus",
+      "Venus",
+      "Jupiter"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Planet which Lies at the Outermost Orbit of the Solar System?",
+    answer: "Neptune",
+    options: [
+      "Neptune",
+      "Saturn",
+      "Uranus",
+      "Mercury"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following is not a Gas Planet?",
+    answer: "Venus",
+    options: [
+      "Jupiter",
+      "Uranus",
+      "Neptune",
+      "Venus"
+    ]
+  },
+  {
+    numb: 11,
+    question: "How long is One Year on Jupiter?",
+    answer: "12 Earth years",
+    options: [
+      "11 Earth years",
+      "17 Earth years",
+      "12 Earth years",
+      "20 Earth years"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which is the Oldest Planet in our Solar System?",
+    answer: "Jupiter",
+    options: [
+      "Jupiter",
+      "Mars",
+      "Earth",
+      "Mercury"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which Planet Spins Backward Relative to the others?",
+    answer: "Venus",
+    options: [
+      "Earth",
+      "Venus",
+      "Neptune",
+      "Mars"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which Planet has the Fastest Rotation?",
+    answer: "Jupiter",
+    options: [
+      "Mercury",
+      "Mars",
+      "Venus",
+      "Jupiter"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Planet which is Called as the Red Planet?",
+    answer: "Mars",
+    options: [
+      "Sun",
+      "Moon",
+      "Mars",
+      "Saturn"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Unscramble the Word 'ARTHE'.",
+    answer: "EARTH",
+    options: [
+      "ERATH",
+      "EARTH",
+      "HTRAE",
+      "RHTEA"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Unscramble the Word 'MONO'.",
+    answer: "MOON",
+    options: [
+      "MOON",
+      "NOOM",
+      "NOMO",
+      "OONM"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Unscramble the Word 'SNUVE'.",
+    answer: "VENUS",
+    options: [
+      "SUNEV",
+      "VSNUE",
+      "VENUS",
+      "UNSVE"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Unscramble the Word 'ARMS'.",
+    answer: "MARS",
+    options: [
+      "SRAM",
+      "AMRS",
+      "MASR",
+      "MARS"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Unscramble the Word 'PSECA'.",
+    answer: "SPACE",
+    options: [
+      "ECAPS",
+      "SPACE",
+      "ACPSE",
+      "ESPAC"
+    ]
+  },
+];
